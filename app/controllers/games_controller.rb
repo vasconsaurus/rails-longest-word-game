b3a2b959014903b0e465d6467cbf8aac5e62d227
manu vasconcelos <manu.vasconcelos@gmail.com>
2022-02-21 20:10:03 -0300

    first version

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -6,8 +6,17 @@ class GamesController < ApplicationController
   end
 
   def score
-    @word_exists = URI.open("https://wagon-dictionary.herokuapp.com/#{params[:word]}")
-    @word_exists = JSON.parse(@word_exists.read)
-    @word_exists = @word_exists["found"]
+    @api_response = JSON.parse(URI.open("https://wagon-dictionary.herokuapp.com/#{params[:word]}").read)
+    @word_exists = @api_response["found"]
+
+    # @contains_letters = true
+    # params[:word].split("").each do |char|
+    #   @contains_letters = params[:letters].include?(char)
+    #   break unless @contains_letters
+    # end
+
+    @contains_letters = params[:word].split('').all? { params[:letters].include?(_1) }
+
+    @has_won = @word_exists && @contains_letters
   end
 end
